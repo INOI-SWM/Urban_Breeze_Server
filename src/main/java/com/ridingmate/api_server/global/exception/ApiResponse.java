c79c292638c1120df161ca0ba8ce83a37df13de5
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-06-27 16:01:16 +0900

    feat: SuccessCode에서 code 필드가 Null일때 생략하도록 변경

--- src/main/java/com/ridingmate/api_server/global/exception/ApiResponse.java
+++ src/main/java/com/ridingmate/api_server/global/exception/ApiResponse.java
@@ -1,7 +1,7 @@
 package com.ridingmate.api_server.global.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
-import org.springframework.validation.BindingResult;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -10,14 +10,17 @@ import org.springframework.validation.BindingResult;
 public class ApiResponse<T> {
 
     private int status;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String code;
+
     private String message;
+
     private T data;
 
     public static <T> ApiResponse<T> success(SuccessCode successCode, T data) {
         return ApiResponse.<T>builder()
                 .status(successCode.getStatus().value())
-                .code(successCode.getCode())
                 .message(successCode.getMessage())
                 .data(data)
                 .build();
