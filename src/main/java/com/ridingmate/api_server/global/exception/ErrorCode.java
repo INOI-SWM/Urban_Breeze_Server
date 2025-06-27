697d429b4857d78913b1d09c55956d7341ebcb9c
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-06-27 14:38:18 +0900

    feat: 에러 코드 인터페이스 정의 및 글로벌 에러 구현

---
+++ src/main/java/com/ridingmate/api_server/global/exception/ErrorCode.java
@@ -0,0 +1,11 @@
+package com.ridingmate.api_server.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    HttpStatus getStatus();
+
+    String getCode();
+
+    String getMessage();
+}
