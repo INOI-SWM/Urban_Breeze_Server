ec07c82f79dbcba65a95106846f64d4049510db7
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-06-27 16:00:33 +0900

    feat: ErrorCode와 SuccessCode가 BaseCode를 상속하도록 변경

--- src/main/java/com/ridingmate/api_server/global/exception/BaseCode.java
+++ src/main/java/com/ridingmate/api_server/global/exception/BaseCode.java
@@ -2,6 +2,8 @@ package com.ridingmate.api_server.global.exception;
 
 import org.springframework.http.HttpStatus;
 
-public interface ErrorCode extends BaseCode {
-    String getCode();
+public interface BaseCode {
+    HttpStatus getStatus();
+
+    String getMessage();
 }
--- src/main/java/com/ridingmate/api_server/global/exception/SuccessCode.java
+++ src/main/java/com/ridingmate/api_server/global/exception/SuccessCode.java
@@ -1,20 +1,4 @@
 package com.ridingmate.api_server.global.exception;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-
-@Getter
-@RequiredArgsConstructor
-public enum SuccessCode {
-    SELECT_SUCCESS(HttpStatus.OK, "200", "조회 요청이 성공했습니다."),
-    DELETE_SUCCESS(HttpStatus.OK, "200", "삭제 요청이 성공했습니다."),
-    INSERT_SUCCESS(HttpStatus.CREATED, "201", "삽입 요청이 성공했습니다."),
-    UPDATE_SUCCESS(HttpStatus.NO_CONTENT, "204", "업데이트 요청이 성공했습니다.")
-    ;
-
-    private final HttpStatus status;
-    private final String code;
-    private final String message;
+public interface SuccessCode extends BaseCode{
 }
-
