d658ce8b28e08597a8a527d44b80ce96ffd04ada
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-06-30 14:43:28 +0900

    fix: cors 서버 경로 수정

--- src/main/java/com/ridingmate/api_server/global/config/WebConfig.java
+++ src/main/java/com/ridingmate/api_server/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ridingmate.api_server.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${swagger.server-url}")
+    private String serverUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS")
+        registry.addMapping("/api/**")
+                .allowedOrigins(serverUrl)
+                .allowedMethods("*")
+                .allowCredentials(true);
+
+        registry.addMapping("/v3/api-docs/**")
+                .allowedOrigins(serverUrl)
+                .allowedMethods("*")
                 .allowCredentials(true);
     }
 }
