d0e6300e3f2ceaf90ff5e2356c73d70e9c1b7444
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-06-30 13:50:25 +0900

    chore: 요청 dto에 schema 예시 추가

--- src/main/java/com/ridingmate/api_server/domain/route/dto/request/RouteSegmentRequest.java
+++ src/main/java/com/ridingmate/api_server/domain/route/dto/request/RouteSegmentRequest.java
@@ -1,11 +1,24 @@
 package com.ridingmate.api_server.domain.route.dto.request;
 
 import com.ridingmate.api_server.global.client.dto.request.OrsRouteRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record RouteSegmentRequest(
+        @Schema(
+                description = "경로를 구성하는 좌표 리스트 [경도, 위도] 형식",
+                example = "[[127.012345, 37.567890], [127.045678, 37.589012]]"
+        )
+        @NotNull
         List<List<Double>> coordinates,
+
+        @Schema(
+                description = "고도 포함 여부",
+                example = "true"
+        )
+        @NotNull
         boolean elevation
 ) {
     public OrsRouteRequest toOrsRequest() {
