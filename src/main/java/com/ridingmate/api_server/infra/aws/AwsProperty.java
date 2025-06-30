a74e6921258e673c326d1876092624b6301fc8da
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-06-30 16:22:04 +0900

    feat: aws 설정값 저장 클래스 생성

---
+++ src/main/java/com/ridingmate/api_server/infra/aws/AwsProperty.java
@@ -0,0 +1,19 @@
+package com.ridingmate.api_server.infra.aws;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import software.amazon.awssdk.regions.Region;
+
+@ConfigurationProperties(prefix = "cloud.aws")
+public record AwsProperty(
+        @NotBlank String region,
+        Credentials credentials,
+        S3 s3
+) {
+    public record Credentials(@NotBlank String accessKey, @NotBlank String secretKey) {}
+    public record S3(@NotBlank String bucket) {}
+
+    public Region regionAsEnum() {
+        return Region.of(region); // Region.of는 유효한 region 문자열만 허용
+    }
+}
