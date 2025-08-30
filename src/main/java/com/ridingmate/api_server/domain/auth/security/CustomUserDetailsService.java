c0db88a245ede3f7973ca77ac9167c5b21498836
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-08-30 22:19:13 +0900

    feat: AuthenticationPrincipal을 통해 정보를 불러올 수 있도록 CustomUserDetails 클래스 생성

---
+++ src/main/java/com/ridingmate/api_server/domain/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.ridingmate.api_server.domain.auth.security;
+
+import com.ridingmate.api_server.domain.user.entity.User;
+import com.ridingmate.api_server.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 이메일을 가진 유저를 찾을 수 없습니다: " + username));
+
+        return CustomUserDetails.from(user);
+    }
+}
